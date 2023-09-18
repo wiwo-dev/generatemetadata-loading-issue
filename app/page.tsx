881ec09b1b4b42821695992a4356050101b85227
wiwo-one <wojtekwieclawski@gmail.com>
2023-09-18 19:18:58 +0700

    keys

--- app/page.tsx
+++ app/page.tsx
@@ -11,31 +11,31 @@ export default async function UsersPage() {
   const users = await usersData;
 
   const content = (
-    <section>
+    <div>
       <br />
       <h2>Users (with generateMetadata) - it hangs for 3 seconds after clicking. No loading state...</h2>
-      {users.map((user) => {
+      {users.map((user, key) => {
         return (
-          <>
+          <div key={key}>
             <p key={user.id}>
               <Link href={`/users/${user.id}`}>{user.name}</Link>
             </p>
             <br />
-          </>
+          </div>
         );
       })}
       <h2>Users (NO metadata) - works fine. Suspense works and there is loading state.</h2>
-      {users.map((user) => {
+      {users.map((user, key) => {
         return (
-          <>
+          <div key={key}>
             <p key={user.id}>
               <Link href={`/users-no-metadata/${user.id}`}>{user.name}</Link>
             </p>
             <br />
-          </>
+          </div>
         );
       })}
-    </section>
+    </div>
   );
 
   return content;
